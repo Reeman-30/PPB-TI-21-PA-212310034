a19b56ef4d6f239641ab24c9d626164f3c80084d
Reeman-30 <rayyanpratama30@gmail.com>
2023-04-04 13:09:40 +0700

    Pertemuan 1

---
+++ MyProfileApp/App.js
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Profile from "./src/components/Profile";
+
+export default function App() {
+	return <Profile />;
+}
